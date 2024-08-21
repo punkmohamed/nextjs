675d6aa6dab002480c6a6b6b3d7191ecbe5cf7b0
punkmohamed <punkmohamed22@gmail.com>
2024-08-21 16:55:11 +0200

    deployment

--- src/app/api/post/[id]/route.js
+++ src/app/api/post/[id]/route.js
@@ -3,12 +3,12 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function GET(request) {
-    const id = request.nextUrl.pathname.split('/').pop();
-    const client = await clientPromise;
-    const db = client.db();
-    const post = await db.collection("nextData").findOne({ id: id });
-    if (!post) {
-        return new Response("Not Found", { status: 404 });
-    }
-    return new Response(JSON.stringify(post), { status: 200 });
+    // const id = request.nextUrl.pathname.split('/').pop();
+    // const client = await clientPromise;
+    // const db = client.db();
+    // const post = await db.collection("nextData").findOne({ id: id });
+    // if (!post) {
+    //     return new Response("Not Found", { status: 404 });
+    // }
+    // return new Response(JSON.stringify(post), { status: 200 });
 }
